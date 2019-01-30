f4ebbaaea7040ee6e46ff2e19764832bb34c7afa
GucciLee <guccilee@163.com>
2019-01-30 18:22:08 +0800

    upload imag

--- pages/insert/insert.js
+++ pages/insert/insert.js
@@ -12,6 +12,7 @@ Page({
       image: ''
     },
   },
+  // 表单提交
   submit: function(){
     api.authRequest({
       url: 'products',
@@ -21,31 +22,42 @@ Page({
       console.log(res)
     })
   },
-
-  showTopTips: function () {
-    var that = this;
-    this.setData({
-      showTopTips: true
-    });
-    setTimeout(function () {
-      that.setData({
-        showTopTips: false
-      });
-    }, 3000);
-  },
-  chooseImage: function (e) {
+  // 图片上传
+  updateImage: function (e) {
     var that = this;
     wx.chooseImage({
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        that.setData({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (image) {
+        // 获取选择的图片
+        let product_img = image.tempFilePaths[0]
+
+        api.updateFile({
+          url: 'images',
+          method: 'POST',
+          name: 'image',
+          formData: {
+            type: 'avatar'
+          },
+          filePath: product_img,
+          complete: function (imageResponse){
+            console.log(imageResponse)
+            // 上传成功成功记录数据
+            if (imageResponse.statusCode === 201) {
+              // 小程序上传结果没有做 JSON.parse，需要手动处理
+              let responseData = JSON.parse(imageResponse.data)
+              console.log(responseData);
+            }
+          }
+        })
+        /*that.setData({
           files: that.data.files.concat(res.tempFilePaths)
-        });
+        });*/
       }
     })
   },
+  // 展示大图
   previewImage: function (e) {
     wx.previewImage({
       current: e.currentTarget.id, // 当前显示图片的http链接
